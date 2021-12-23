0555fe391a89755f6010de3d661cdfebd35b7f01
Karina <aiwpront@gmail.com>
2021-12-23 14:06:20 +0400

    added TestBase.java

---
+++ src/test/java/com/github/jjfhj/tests/TestBase.java
@@ -0,0 +1,32 @@
+package com.github.jjfhj.tests;
+
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import com.github.jjfhj.config.WebDriverUtil;
+import com.github.jjfhj.helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+
+import static io.qameta.allure.Allure.step;
+
+public class TestBase {
+
+    @BeforeAll
+    static void setup() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        WebDriverUtil.configure();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+
+        step("Закрыть браузер", () -> {
+            Selenide.closeWebDriver();
+        });
+    }
+}
