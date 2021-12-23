ef729711b08049b4b25d6a4a933ba9ecdb043199
Karina <aiwpront@gmail.com>
2021-12-23 14:05:38 +0400

    added WebDriverConfig.java

---
+++ src/test/java/com/github/jjfhj/config/WebDriverConfig.java
@@ -0,0 +1,19 @@
+package com.github.jjfhj.config;
+
+import org.aeonbits.owner.Config;
+
+@Config.Sources({"classpath:config/locale.properties"})
+public interface WebDriverConfig extends Config{
+
+    @Key("browser")
+    @DefaultValue("chrome")
+    String browser();
+
+    @Key("versionBrowser")
+    @DefaultValue("91.0")
+    String versionBrowser();
+
+    @Key("browserSize")
+    @DefaultValue("1920x1080")
+    String browserSize();
+}
